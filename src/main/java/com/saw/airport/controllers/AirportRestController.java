3a72e01a3aa241feec671d52fa5c4d0f640a0e46
vortiz86 <vortiz86@gmail.com>
2021-07-20 14:26:55 -0400

    Activacion de cors

--- src/main/java/com/saw/airport/controllers/AirportRestController.java
+++ src/main/java/com/saw/airport/controllers/AirportRestController.java
@@ -3,15 +3,19 @@ package com.saw.airport.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saw.airport.entities.Airport;
 import com.saw.airport.service.ApiService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/api")
+
 public class AirportRestController {
 
 	@Autowired
